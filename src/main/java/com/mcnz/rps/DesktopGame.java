155777a925c0657bd3a869f2cc3cbe887a0022fa
nlane510 <119709227+nlane510@users.noreply.github.com>
2024-02-13 13:14:56 -0600

    Update DesktopGame.java

--- src/main/java/com/mcnz/rps/DesktopGame.java
+++ src/main/java/com/mcnz/rps/DesktopGame.java
@@ -1,45 +1,45 @@
 package com.mcnz.rps;
 
 import javax.swing.JOptionPane;
+import java.util.Vector;
 
 public class DesktopGame {
-	public enum Gesture {rock,paper,scissors};
+    public enum Gesture { ROCK, PAPER, SCISSORS }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        String prompt = "Will it be rock, paper, or scissors?";
+        Score score = new Score();
+        Vector<GameSummary> history = new Vector<>();
 
-		String prompt = "Will it be rock, paper or scissors?";
-		Score score = new Score();
-		java.util.Vector<GameSummary> history = new java.util.Vector<GameSummary>();
+        while (true) {
+            String result = "error";
+            String input = JOptionPane.showInputDialog(prompt);
 
-		while (true) {
-			String result = "error";
-			String input = JOptionPane.showInputDialog(prompt);
-			if (input.equals(Gesture.scissors.toString())) {
-				result = "lose";
-				score.increaseLosses();
-			}
-			if (input.equals(Gesture.paper.toString())) {
-				result = "win";
-				score.increaseWins();
-			}
-			if (input.equals(Gesture.rock.toString())) {
-				result = "tie";
-				score.increaseTies();
-			}
-			if (input.equalsIgnoreCase("quit")) {
-				break;
-			}
-			GameSummary gs = new GameSummary(input, "rock", result);
-			history.add(gs);
+            switch (input.toLowerCase()) {
+                case "scissors":
+                    result = "lose";
+                    score.increaseLosses();
+                    break;
+                case "paper":
+                    result = "win";
+                    score.increaseWins();
+                    break;
+                case "rock":
+                    result = "tie";
+                    score.increaseTies();
+                    break;
+                case "quit":
+                    return;
+            }
 
-			JOptionPane.showMessageDialog(null, result);
-			System.out.println(score);
-			for (GameSummary gameSummary : history) {
-				System.out.println(gameSummary);
-			}
-			
-		}
-	}
+            GameSummary gs = new GameSummary(input, "rock", result);
+            history.add(gs);
+
+            JOptionPane.showMessageDialog(null, result);
+            System.out.println(score);
+            for (GameSummary gameSummary : history) {
+                System.out.println(gameSummary);
+            }
+        }
+    }
 }
-
-
